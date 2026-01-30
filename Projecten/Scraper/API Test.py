f3b00fc264ff33a67324d879ad8f673d7c10678b
Joel <158784536+JoelvHouten@users.noreply.github.com>
2026-01-30 22:12:31 +0100

    Refactor API Test.py to remove hardcoded API key

--- Projecten/Scraper/API Test.py
+++ Projecten/Scraper/API Test.py
@@ -1,6 +1,6 @@
 import requests
 
-api_key = "4e2992e109ff54af27913f400b05c236"
+api_key = ""
 url = f"https://api.themoviedb.org/3/movie/550?api_key={api_key}"
 
 response = requests.get(url)
@@ -8,3 +8,4 @@ if response.status_code == 200:
     print("API request successful!")
 else:
     print(f"Request failed with status code: {response.status_code}")
+
